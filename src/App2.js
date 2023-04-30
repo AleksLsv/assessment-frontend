7bedf3c04c820c7a8bd551dfe961b121dacbc723
Aleksandr <jooksja1@gmail.com>
2023-04-30 12:03:23 +0300

    App2, data from file added

--- src/App2.js
+++ src/App2.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { fetchShipmentsData } from './actions/actions';
+import {connect} from 'react-redux';
+import {fetchShipmentsData} from './actions/actions';
 import {Route, Routes} from "react-router-dom";
 import ShipmentsTable from "./components/ShipmentsTable";
 import MyForm from "./components/MyForm";
-import Test1 from "./components/Test1";
-import Test2 from "./components/Test2";
 
 class App extends React.Component {
     componentDidMount() {
@@ -13,24 +11,30 @@ class App extends React.Component {
     }
 
     render() {
-        const { error, loading, shipments } = this.props;
+        const {error, loading, shipments} = this.props;
 
-        if (error) {
-            return <div>Error: {error.message}</div>;
-        }
 
         if (loading) {
             return <div>Loading...</div>;
         }
 
-        return (
-/*            <ul>
-                {shipments.map((shipment) => (
-                    <li key={shipment.orderNo}>{shipment.customer}</li>
-                ))}
-            </ul>*/
+        if (error) {
+            const shipments = require('./assets/data/shipments.json');
+            return (
+                    <div className="App">
+                        <h4>Error: {error.message}  - Data loaded from file  </h4>
+                        <Routes>
+                            <Route path="/" element={<ShipmentsTable data={shipments}/>}/>
+                            <Route path="/form" element={<MyForm/>}/>
+                        </Routes>
+                    </div>
+            );
+        }
 
+        return (
             <div className="App">
+
+                <h4>Connection successful - Data loaded from server</h4>
 
                 <Routes>
                     <Route path="/" element={<ShipmentsTable data={shipments}/>}/>
@@ -43,7 +47,7 @@ class App extends React.Component {
 }
 
 function mapStateToProps(state) {
-    const { error, loading, shipments } = state;
+    const {error, loading, shipments} = state;
     return {
         error,
         loading,
